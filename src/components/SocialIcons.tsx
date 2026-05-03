1e51979d132cd3627fd03a204509b955a1872043
Prajwal Singh Rajput <106654579+prajwal00017@users.noreply.github.com>
2026-05-03 19:46:05 +0530

    Update resume link to point to new PDF

--- src/components/SocialIcons.tsx
+++ src/components/SocialIcons.tsx
@@ -74,7 +74,7 @@ const SocialIcons = () => {
           </a>
         </span>
       </div>
-      <a className="resume-button" href="/Resume_Prajwal.pdf" target="_blank" rel="noopener noreferrer">
+      <a className="resume-button" href="/Resume_PRS.pdf" target="_blank" rel="noopener noreferrer">
         <HoverLinks text="RESUME" />
         <span>
           <TbNotes />
